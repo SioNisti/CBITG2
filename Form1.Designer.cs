c2439b91345d63315a62448667f48d5e3178cdc7
SioNisti <qmenat@gmail.com>
2023-01-09 23:01:24 +0200

    history

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,7 +32,13 @@ namespace CBITG2
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.fIToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eNToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.label1 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // notifyIcon1
@@ -40,30 +46,71 @@ namespace CBITG2
             this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
             this.notifyIcon1.BalloonTipText = "aaaaa";
             this.notifyIcon1.BalloonTipTitle = "bbbbb";
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
             this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
             this.notifyIcon1.Text = "notifyIcon1";
             this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.fIToolStripMenuItem,
+            this.eNToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(94, 92);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(93, 22);
+            this.toolStripMenuItem1.Text = "JP";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // fIToolStripMenuItem
+            // 
+            this.fIToolStripMenuItem.Name = "fIToolStripMenuItem";
+            this.fIToolStripMenuItem.Size = new System.Drawing.Size(93, 22);
+            this.fIToolStripMenuItem.Text = "FI";
+            this.fIToolStripMenuItem.Click += new System.EventHandler(this.fIToolStripMenuItem_Click);
+            // 
+            // eNToolStripMenuItem
+            // 
+            this.eNToolStripMenuItem.Name = "eNToolStripMenuItem";
+            this.eNToolStripMenuItem.Size = new System.Drawing.Size(93, 22);
+            this.eNToolStripMenuItem.Text = "EN";
+            this.eNToolStripMenuItem.Click += new System.EventHandler(this.eNToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(93, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(173, 13);
+            this.label1.Size = new System.Drawing.Size(191, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Minimize window to put it in the tray";
+            this.label1.Text = "Minimize the window to put it in the tray";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(193, 45);
+            this.ClientSize = new System.Drawing.Size(214, 37);
             this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "CBITG2";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -73,6 +120,11 @@ namespace CBITG2
 
         private System.Windows.Forms.NotifyIcon notifyIcon1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem fIToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eNToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
+using System.IO;
+using System.Linq;
 using System.Windows.Forms;
 using Tesseract;
 
@@ -11,24 +14,33 @@ namespace CBITG2
         public Form1()
         {
             InitializeComponent(); 
-            this.notifyIcon1.Text = "M1 to get text\nM2 to close the program";
+            this.notifyIcon1.Text = "M1 to get text (eng,fin,jpn)\nM2 to open menu";
         }
+        List<string> historyL = new List<string>();
 
         private void Form1_Load(object sender, EventArgs e)
         {
             notifyIcon1.Visible = true;
+            if (!File.Exists("history.txt"))
+            {
+                File.Create("history.txt"); 
+                Application.Restart();
+                Environment.Exit(0);
+            }
+            historyL = System.IO.File.ReadLines("history.txt").ToList();
         }
 
-        public void Recognize()
+        public void Recognize(string lang)
         {
             try
             {
                 Bitmap bmp = new Bitmap(Clipboard.GetImage());
                 Pix img = PixConverter.ToPix(bmp);
-                TesseractEngine engine = new TesseractEngine("./tessdata", "jpn+eng+fin", EngineMode.LstmOnly);
+                TesseractEngine engine = new TesseractEngine("./tessdata", lang, EngineMode.LstmOnly);
                 Page page = engine.Process(img, PageSegMode.Auto);
                 Clipboard.SetText(page.GetText());
                 label1.Text = page.GetText();
+                historyL.Add($"({DateTime.Now} [{lang}]){page.GetText()}");
             }
             catch (Exception e)
             {
@@ -36,23 +48,44 @@ namespace CBITG2
                 Console.WriteLine("Unexpected Error: " + e.Message);
                 Console.WriteLine("Details: ");
                 Console.WriteLine(e.ToString());
+                label1.Text = e.ToString();
+                historyL.Add($"({DateTime.Now} [{lang}]){e.ToString()}");
             }
+            System.IO.File.WriteAllLines("history.txt", historyL);
         }
 
         private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
         {
             if (e.Button == MouseButtons.Left)
             {
-                Recognize();
-            } else
-            {
-                this.Close();
+                Recognize("jpn+eng+fin");
             }
         }
 
         private void Form1_Resize(object sender, EventArgs e)
         {
             this.Hide();
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Recognize("jpn");
+        }
+
+        private void fIToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            Recognize("fin");
+        }
+
+        private void eNToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Recognize("eng");
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
